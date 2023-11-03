998fbd7c4f118957f8faa755267a9dbbcd7f6f97
Robert Waffen <rw@betadots.de>
2023-11-03 13:07:47 +0100

    update syntax on how to exec the batchfile

--- lib/puppet/provider/wildfly_cli_batchfile/http_api.rb
+++ lib/puppet/provider/wildfly_cli_batchfile/http_api.rb
@@ -4,8 +4,8 @@ Puppet::Type.type(:wildfly_cli_batchfile).provide :http_api, :parent => Puppet::
   desc 'Uses JBoss HTTP API to execute a JBoss-CLI batchfile'
 
   def exec_command
-    debug "Running: #{@resource[:path]}"
-    cli.exec(@resource[:path])
+    debug "Running: run-batch --file=#{@resource[:path]}"
+    cli.exec("run-batch --file=#{@resource[:path]}")
   end
 
   def should_execute?
